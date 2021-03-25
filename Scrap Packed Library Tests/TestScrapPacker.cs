e45fdd4ff40c0c48fcace2598826e10d83e622f4
romibi <romibi@bluewin.ch>
2021-03-25 16:45:01 +0100

    Create Library Unit Tests project

---
+++ Scrap Packed Library Tests/TestScrapPacker.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ch.romibi.Scrap.Packed.PackerLib.Tests
+{
+    [TestClass]
+    public class TestScrapPacker
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.Fail("no tests implemented");
+        }
+    }
+}
