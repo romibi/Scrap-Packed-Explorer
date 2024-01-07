89cd9d8f568c267c867f796282dd9025d4578e0b
romibi <romibi@bluewin.ch>
2024-01-07 02:47:37 +0100

    First draft of Dark mode support
    Not very happy with how it's implemented …
    Also maybe UI will be redone in some multiplatform framework …
    Meh … i'll tinker a bit more with this but idk …

--- ScrapPackedExplorer/GuiApp.xaml.cs
+++ ScrapPackedExplorer/GuiApp.xaml.cs
@@ -4,18 +4,34 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using System.Windows;
 
 namespace ch.romibi.Scrap.Packed.Explorer {
+    public enum Theme { System, Dark, Light }
+
+
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class GuiApp : Application {
         protected string PackedFilePath { get; set; }
 
+        private Theme _AppTheme = Theme.System;
+        public Theme AppTheme {
+            get { return _AppTheme; }
+            set {
+                _AppTheme = value;
+                UpdateTheme();
+            }
+        }
+
         protected override void OnStartup(StartupEventArgs e) {
             base.OnStartup(e);
+
+            UpdateTheme();
+
             MainWindow mainWindow = new MainWindow();
 
             if (PackedFilePath != null) {
@@ -29,5 +45,24 @@ namespace ch.romibi.Scrap.Packed.Explorer {
         public void LoadPackedFile(string p_PackedPath) {
             PackedFilePath = p_PackedPath;
         }
+
+        private void UpdateTheme() {
+            bool dark;
+            if (AppTheme == Theme.System) {
+                dark = ShouldSystemUseDarkMode();
+            } else {
+                dark = AppTheme == Theme.Dark;
+            }
+
+            Resources.Clear();
+            if (dark) {
+                Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("/Themes/DarkTheme.xaml", UriKind.Relative) });
+            } else {
+                Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("/Themes/LightTheme.xaml", UriKind.Relative) });
+            }
+        }
+
+        [DllImport("UXTheme.dll", SetLastError = true, EntryPoint = "#138")]
+        private static extern bool ShouldSystemUseDarkMode();
     }
 }
